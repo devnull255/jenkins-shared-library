46e29965f78c2e6c3c6ff150511be26b17963c96
Michael Mabin <mabinm@comcast.net>
2021-02-21 13:48:27 -0600

    Initial commit

---
+++ src/com/example/jenkins/Constants.groovy
@@ -0,0 +1,6 @@
+package com.example.jenkins
+
+class Constants {
+   static final String SLACK_MESSAGE = "Sending Slack Notification"
+   static final String EMAIL_MESSAGE = "Sending Email"
+}
---
+++ vars/notify.groovy
@@ -0,0 +1,12 @@
+/* notify.groovy */
+import com.example.jenkins.Constants
+
+def call(Map config=[:]) {
+    if (config.type == "slack") {
+        echo Constants.SLACK_MESSAGE
+        echo config.message
+    } else {
+        echo Constants.EMAIL_MESSAGE
+        echo config.message
+    }
+}
